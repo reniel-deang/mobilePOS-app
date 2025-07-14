af7cc20c8233e04358aad3f536057bc53dd4c5f7
reniel-deang <xexxdump@gmail.com>
2025-07-15 01:10:59 +0800

    Change hostaddress.dart with added note

--- lib/variable/hostaddress.dart
+++ lib/variable/hostaddress.dart
@@ -1,4 +1,7 @@
 //Change here the host if you want to change the api server
 
-final String hostaddress = "http://192.168.8.41:8000";
+// hostaddress = "http://10.0.2.2:8000" if you want to test it in Android Emulator
+// If you want to test in real device. Please look for ipv4 of the server with the port 8000
+
+final String hostaddress = "http://10.0.2.2:8000";
 String? token;
